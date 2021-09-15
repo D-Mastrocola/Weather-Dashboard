ee29521421b8a7acf194e1576aeeabe7ed0cb2b6
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-09-15 16:39:46 -0400

    added api key

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,5 @@
-console.log('test')
+const apiKey = '789f2c7c553ae281ecf566fcce879323';
+
+let response = fetch('https://api.openweathermap.org/data/2.5/onecall?q=London&appid=' + apiKey);
+
+console.log(response)
