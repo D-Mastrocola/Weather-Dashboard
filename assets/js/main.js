0b7cdf4d5e1d1cdbc78267ffc0fabf47e6c2c9ee
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-09-15 16:45:53 -0400

    makes a call to api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
 const apiKey = '789f2c7c553ae281ecf566fcce879323';
 
-let response = fetch('https://api.openweathermap.org/data/2.5/onecall?q=London&appid=' + apiKey);
+let response = fetch('https://api.openweathermap.org/data/2.5/onecall?lat=60.99&lon=30.9&appid=' + apiKey)
+.then((response) => {
+    // request was successful
+    if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data)
+        });
+      } else {
+        alert("Error: could not retrieve weather data");
+      }
+});
 
-console.log(response)
