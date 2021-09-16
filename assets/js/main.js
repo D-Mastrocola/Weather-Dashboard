b0f4ab39d1a5e2d3cd020e4c9ab56cbd94bc366e
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-09-16 18:16:00 -0400

    vfdgsdfg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,34 @@
 const apiKey = '789f2c7c553ae281ecf566fcce879323';
 
-let response = fetch('https://api.openweathermap.org/data/2.5/onecall?lat=60.99&lon=30.9&appid=' + apiKey)
-.then((response) => {
-    // request was successful
-    if (response.ok) {
+
+
+let getWeatherData = (coord) => {
+  let response = fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + coord.lat + '&lon=' + coord.lon + '&appid=' + apiKey)
+    .then((response) => {
+      // request was successful
+      if (response.ok) {
         response.json().then(function (data) {
           console.log(data)
         });
       } else {
         alert("Error: could not retrieve weather data");
       }
-});
+    });
+}
 
+let getCityCoordinates = (city) => {
+  let response = fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+    .then((response) => {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data)
+          getWeatherData(data.coord)
+        });
+      } else {
+        alert("Error: could not retrieve weather data");
+      }
+    });
+}
+
+getCityCoordinates('Cleveland')
